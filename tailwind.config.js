04b5ddbaeb9d678631660ded052d19860fcd00d7
Ashraful Alam Nur <mdashrafulalam.nur@gmail.com>
2026-06-07 23:43:21 +0600

    Initial commit - Vibe Sync Project Started

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"], // আপনার HTML ফাইলের পাথ অনুযায়ী এটি পরিবর্তন করতে পারেন
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
