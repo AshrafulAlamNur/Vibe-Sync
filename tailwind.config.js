02e7858e3ee5d9e7f426773d792b616a4145eea4
Ashraful Alam Nur <mdashrafulalam.nur@gmail.com>
2026-06-10 18:18:11 +0600

    Refactor code structure for improved readability and maintainability

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"], // আপনার HTML ফাইলের পাথ অনুযায়ী এটি পরিবর্তন করতে পারেন
+  content: ["./*.{html,js}"], 
   theme: {
     extend: {},
   },
