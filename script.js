2feafbb682e1341b12b2c03e483febd3c05b70d0
Ashraful Alam Nur <mdashrafulalam.nur@gmail.com>
2026-06-12 16:09:45 +0600

    Add music name and type elements to display currently playing track

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+let musics = [
+  {
+    name: "Music 1",
+    type: "Nature",
+    cover: "cover1.jpg",
+    src: "music1.mp3",
+  },
+  {
+    name: "Music 2",
+    type: "Nature",
+    cover: "cover1.jpg",
+    src: "music1.mp3",
+  },
+];
 const modeElements = document.querySelectorAll(".mode");
+const musicName = document.querySelector(".music-name");
+const musicType = document.querySelector(".music-type");
 
 modeElements.forEach((singleMode) => {
   singleMode.addEventListener("click", () => {
@@ -9,4 +25,9 @@ modeElements.forEach((singleMode) => {
     const currentTick = singleMode.querySelector(".tick");
     currentTick.classList.remove("invisible");
   });
+});
+
+musics.forEach((music) => {
+  musicName.innerHTML = music.name;
+  musicType.innerHTML = music.type;
 });
